c5adb09023991ddc3ed3d1ec8402c9a24bc84c8e
Nikolay Tumakov <n.tumakov@tbank.ru>
2025-08-08 13:07:41 +0300

    Revert "feat: add another loss function"
    This reverts commit c11de69277cb8e32f91ca2109b603e1ba46d9722.

--- src/loss/asv.py
+++ src/loss/asv.py
@@ -1,6 +1,5 @@
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
+from torch import nn
 
 
 class CrossEntropyLoss(nn.Module):
@@ -14,31 +13,3 @@ class CrossEntropyLoss(nn.Module):
 
     def forward(self, logits: torch.Tensor, labels: torch.Tensor, **batch):
         return {"loss": self.loss(logits, labels)}
-
-
-class AngularSoftmaxLoss(nn.Module):
-    def __init__(self, gamma=0.0, m=4, s=30.0):
-        super().__init__()
-        self.gamma = gamma
-        self.margin = m
-        self.scale = s
-        self.easy_margin = True
-        self.cos_m = torch.cos(torch.tensor(m))
-        self.sin_m = torch.sin(torch.tensor(m))
-        self.th = torch.cos(torch.pi - m)
-        self.mm = torch.sin(torch.pi - m) * m
-
-    def forward(self, logits: torch.Tensor, labels: torch.Tensor, **batch):
-        cosine = F.normalize(logits, p=2, dim=1)
-        sine = torch.sqrt(1.0 - torch.pow(cosine, 2))
-
-        cos_theta_m = cosine * self.cos_m - sine * self.sin_m
-
-        if self.easy_margin:
-            final = torch.where(cosine > 0, cos_theta_m, cosine)
-        else:
-            final = torch.where(cosine > self.th, cos_theta_m, cosine - self.mm)
-
-        logits = self.scale * final
-
-        return {"loss": F.cross_entropy(logits, labels)}
